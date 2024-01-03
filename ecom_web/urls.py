c233730f3c11b892ba9959232c92528ce42f6bea
khan022 <sk.siam@outlook.com>
2024-01-03 14:51:09 +0600

    v0.1 the base page with links

--- ecom_web/urls.py
+++ ecom_web/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from ecomm.views import index
+from ecomm.views import *
 
 
 urlpatterns = [
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
     path("admin/", admin.site.urls),
 ]
--- ecomm/views.py
+++ ecomm/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'ecomm/index.html')
+
+def contact(request):
+    return render(request, 'ecomm/contact.html')
