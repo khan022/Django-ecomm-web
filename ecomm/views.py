0eb26913a8c13c423175a525bf0243103452de5b
khan022 <sk.siam@outlook.com>
2024-01-03 20:10:28 +0600

    v0.2 adding items and categories

--- ecomm/views.py
+++ ecomm/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from item.models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'ecomm/index.html')
+    items = Item.objects.filter(is_sold = False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'ecomm/index.html', {
+        'categories' : categories,
+        'items' : items,
+    })
 
 def contact(request):
     return render(request, 'ecomm/contact.html')
